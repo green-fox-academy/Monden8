a970349c997e260c66270af50afd628f2db9c879
Soma <soma.feigl@gmail.com>
2019-04-04 17:28:42 +0200

    done A,V

---
+++ week-01/day-4/thirdPartSomething.ts
@@ -0,0 +1,13 @@
+let a:number = 25;
+let b:number = 30;
+let c:number = 35;
+
+let ac:number = a*c;
+let bc:number = b*c;
+let ab:number = a*b;
+
+let A:number = 2*ac + 2*ab + 2* bc;
+console.log("A : " + A);
+
+let V:number = a*b*c;
+console.log("V : " + V)
