0c4a79b2f17800eeb3108bee8f22f7f5f416d60c
Soma <soma.feigl@gmail.com>
2019-04-04 15:19:00 +0200

    added rounded percentage

--- week-01/day-4/something.ts
+++ week-01/day-4/something.ts
@@ -17,18 +17,19 @@
         console.log(n2 / n1);
         console.log(n1 % n2);
 
-            //wasn't only coding on workdays
-
             let codingHours:number = 6;
+            let codingHoursWeekly:number = undefined
             let totalweeks:number = 17;
             let workdays:number = 5;
             let dailyHours:number = 24;
             let worksHoursWeekly:number = 52;
             let sumOfHours:number =undefined;
-            let codingPercentage:number = undefined
 
             console.log(sumOfHours = codingHours * totalweeks * workdays);
             let daysSpentWithHours:number = (sumOfHours / dailyHours);
             console.log("Total days spent with coding: " + daysSpentWithHours );
-            console.log(codingPercentage = sumOfHours / worksHoursWeekly )
+            codingHoursWeekly = (sumOfHours / totalweeks);
+            let numberpercentage:number = ((codingHoursWeekly / worksHoursWeekly) *100);
+            numberpercentage =  Math.floor (numberpercentage)
+            console.log(numberpercentage+ "% ")
 
