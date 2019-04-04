acf6509c1ab31c5dcbdcaa4202b071995e7d69cc
Soma <soma.feigl@gmail.com>
2019-04-04 17:12:33 +0200

    done g,hj,i

--- week-01/day-4/secondPartSomething.ts
+++ week-01/day-4/secondPartSomething.ts
@@ -34,20 +34,43 @@ console.log("My favorite number is: " + faveoriteNumber);
 
                     let q:number = 3;
                     q += 10;
-                    console.log(q)
+                    console.log(q);
                     let w:number = 100;
                     w -= 7;
-                    console.log(w)
+                    console.log(w);
                     let e:number = 44;
                     e *= 2;
-                    console.log(e)
+                    console.log(e);
                     let r:number = 125;
                     r /= 5;
-                    console.log(r)
+                    console.log(r);
                     let t:number = 8; 
                     t **= 2;
-                    console.log(t)
+                    console.log(t);
+
                     let f1:number = 123;
                     let f2:number = 345;
                     let isBigger:boolean =  f1 > f2;
-                    console.log(isBigger)
+                    console.log(isBigger);
+
+                    let g1:number = 350;
+                    let g2:number = 200;
+                    let isItBigger:boolean =  ((g2*2) > g1 );
+                    console.log(isItBigger);
+
+                    let h:number = 1357988018575474;
+                    let j:number = 1357988018575474 % 11 ;
+                    let yolo:boolean = (j === 0);
+                    console.log(yolo);
+
+                    let i1:number = 10;
+                    let i2:number = 3;
+                    let é:boolean = (i1 > i2**2 && i1 < i2**3);
+                    console.log(é);
+                 
+                    
+
+
+
+        
+
