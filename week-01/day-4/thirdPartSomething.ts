9e3a9aa17de5b9760a006094d453e4e576a42a79
Soma <soma.feigl@gmail.com>
2019-04-04 17:47:24 +0200

    seconds in a day

--- week-01/day-4/thirdPartSomething.ts
+++ week-01/day-4/thirdPartSomething.ts
@@ -11,3 +11,14 @@ console.log("A : " + A);
 
 let V:number = a*b*c;
 console.log("V : " + V)
+
+let currentHours:number = 14;
+let currentMinutes:number = 34;
+let currentSeconds:number = 42;
+
+let q: number = 24 - currentHours;
+let w: number = 60 - currentMinutes;
+let e: number = 60 - currentSeconds;
+console.log("Hours left from the day : " + q + 
+"\nMinutes left from the day : " + w +
+"\nSeconds left from the day : " + e );
