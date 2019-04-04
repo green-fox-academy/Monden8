07456828ce1e65cf65703ba4b3e64dd5d41c0d3f
Soma <soma.feigl@gmail.com>
2019-04-04 15:47:52 +0200

    switched the variables(cheated a little first)

--- week-01/day-4/secondPartSomething.ts
+++ week-01/day-4/secondPartSomething.ts
@@ -1,4 +1,22 @@
 'use strict'
 
 let faveoriteNumber:number = 8;
-console.log("My favorite number is: " + faveoriteNumber)
+console.log("My favorite number is: " + faveoriteNumber);
+
+    let a:number = 123;
+    let b:number = 526;
+    
+    console.log("a: " + a);
+    console.log("b: " + b);
+
+    let c:number = (a + b);
+    console.log("a: " + (c - a))
+    console.log("b: " + (c - b))
+
+    a=a+b
+    b=a-b
+    a=a-b 
+    console.log("a: " + a);
+    console.log("b: " + b);
+
+    console.log("b: " + (( a + b ) -a  ))
