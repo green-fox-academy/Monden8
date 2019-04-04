2919a4e02580a7fc43b8b553ce6b42ccb0e5a151
Soma <soma.feigl@gmail.com>
2019-04-04 15:57:55 +0200

    calculated BMI

--- week-01/day-4/secondPartSomething.ts
+++ week-01/day-4/secondPartSomething.ts
@@ -5,18 +5,23 @@ console.log("My favorite number is: " + faveoriteNumber);
 
     let a:number = 123;
     let b:number = 526;
-    
     console.log("a: " + a);
     console.log("b: " + b);
 
     let c:number = (a + b);
-    console.log("a: " + (c - a))
-    console.log("b: " + (c - b))
+    console.log("a: " + (c - a));
+    console.log("b: " + (c - b));
 
-    a=a+b
-    b=a-b
-    a=a-b 
+    a=a+b;
+    b=a-b;
+    a=a-b;
     console.log("a: " + a);
     console.log("b: " + b);
+    console.log("b: " + (( a + b ) -a  ));
 
-    console.log("b: " + (( a + b ) -a  ))
+        let massInKg:number = 80.5;
+        let heightInM:number = 1.89;
+        let BMI:number = massInKg/(heightInM**2);
+        BMI = Number(BMI.toFixed(2));
+
+        console.log(BMI)
