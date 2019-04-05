57486f590accf8ee85bb32a4b6fb74bb2865fa4b
Soma <soma.feigl@gmail.com>
2019-04-05 13:45:20 +0200

    did fizz-buzz

--- week-01/day-5/sixthPartSomething.ts
+++ week-01/day-5/sixthPartSomething.ts
@@ -1,29 +1,39 @@
 'use strict'
-let a:number = undefined;
+let a: number = undefined;
 
-for(a = 0; a<2; a++ ){
+for (a = 0; a < 2; a++) {
     console.log("I won't cheat on Exams!");
 }
 
-for(let b:number =0; b<=10; b++){
-    if ( b%2===0){
+for (let b: number = 0; b <= 10; b++) {
+    if (b % 2 === 0) {
         console.log(b);
     }
 }
 
-let number:number = 15;
-for(let start:number =1; start<=10; start++ ){
-    let c:number = (start*number);
+let number: number = 15;
+for (let start: number = 1; start <= 10; start++) {
+    let c: number = (start * number);
     console.log(`${start} * ${number}
      = ${c}`);
-   // console.log(start + "*" +number + "=" + c);
+    // console.log(start + "*" +number + "=" + c);
 }
-let e:number=5;
-let r:number=0;
-let s:number=undefined
-for(let d:number =1; d<=e; d++){
-    (r+=d);
-    (s=r/d);
+let e: number = 5;
+let r: number = 0;
+let s: number = undefined
+for (let d: number = 1; d <= e; d++) {
+    (r += d);
+    (s = r / d);
 }
 console.log(`Sum:${r}
-Average:${s}` )
+Average:${s}`)
+
+for (let ab: number = 1; ab <= 100; ab++) {
+    if (ab%3===0 && ab%5===0){
+        console.log("Fizz");
+    } else if (ab % 5 === 0) {
+        console.log("Buzz");
+    } else if (ab%3===0){
+    console.log("Fizz-Buzz");
+    } else {console.log(ab)
+    }}
