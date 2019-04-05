9a5651cb519cb82d876440197dbe612fe5136714
Soma <soma.feigl@gmail.com>
2019-04-05 11:34:19 +0200

    made some if codes

---
+++ week-01/day-5/fourthPartSomething.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+let a: number = 25;
+let out: number = 0;
+
+if (a % 2 === 0) {
+    (out++)
+}
+
+console.log(out);
+
+let b: number = 13;
+let out2: string = '';
+
+if (b > 10 && b < 20) {
+    out2 = "Sweet!";
+} else if (b > 10) {
+    out2 = "Less!";
+} else if (b > 20) {
+    out2 = "More!";
+}
+
+console.log(out2);
+
+let c: number = 123;
+let credits: number = 50;
+let isBonus: boolean = true;
+
+if (credits >= 50 && !isBonus) {
+    (c -= 2);
+}
+else if(credits < 50 && !isBonus){
+    (c--);
+}
+else if ( credits===50 && isBonus){
+    (c++)
+}
+
+    console.log(c);
+
+let d: number = 9;
+let time: number = 220;
+let out3: string = '';
+
+if ( d%4 === 0 && time <=200){
+    (out3 = "check");
+} else if ( d%4 === 0 && time >200){
+    (out3 = "Time out")
+} else  {
+    (out3 = "Run Forest Run!")
+
+}
+
+console.log(out3);
