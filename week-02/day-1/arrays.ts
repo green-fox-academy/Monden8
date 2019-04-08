79152cef3faff6cd55d69d4e5cccd8e9b0d7c72d
Soma <soma.feigl@gmail.com>
2019-04-08 14:16:03 +0300

    added names-stillNotClear

--- week-02/day-1/arrays.ts
+++ week-02/day-1/arrays.ts
@@ -1,9 +1,26 @@
 'use strict'
 
-let yolo:number = 123;
+let x:number = 123;
 var a:number= undefined;
 function doubling(x:number,y:number){
-    return (a= x * y )
+    return (a= x * y );
 }
-console.log(doubling (123,2))
-console.log (a)
+console.log(doubling (123,2));
+console.log (a);
+
+
+    let idfc:string= "Miért";
+    function greetByNames(greeet = "Hi" , names = 'Pal') {
+        console.log(greeet, names);
+      }
+      
+      greetByNames(idfc ,"Soma");
+      greetByNames('Greetings, Dear', 'Greenfox');
+
+      function makeGreen(name: string): string {
+        let newName = 'Green ' + name;
+        return newName;
+      }
+      
+      let names = makeGreen('Soma');
+      greetByNames('Mizujs?', names);
