98aa9923fcd028bb316b05c4960579a50d057ff9
Soma <soma.feigl@gmail.com>
2019-04-08 13:24:55 +0300

    copied pyramid

--- week-02/weekend+/geogebra.ts
+++ week-02/weekend+/geogebra.ts
@@ -1,1 +1,17 @@
-console.log("hello")
+'use strict';
+
+const star:string = "*";
+const space:string = " ";
+const numberoflines:number = 6;
+
+let numberofstart:number = 1;
+let numberofspaces:number = numberoflines -1;
+
+for(let i =0; i < numberoflines ; i++){
+    let lineofpyramid: string = space.repeat(numberofspaces) + star.repeat(numberofstart);
+    console.log(lineofpyramid)
+    numberofstart += 2;
+    numberofspaces--;
+}
+
+export {};
