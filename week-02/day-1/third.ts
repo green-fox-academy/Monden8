1340ad786786d095ff5a21057d492f122e9226d2
Soma <soma.feigl@gmail.com>
2019-04-08 15:59:24 +0300

    did the first 3 excercises

---
+++ week-02/day-1/third.ts
@@ -0,0 +1,25 @@
+'use strict'
+
+
+// -  Create a variable named `magicNumbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `magicNumbers`
+
+let magicNumbers=[1,3,5,7];
+console.log(magicNumbers[2]);
+
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+
+let firstList=[1,2,3];
+let secondList=[4,5];
+console.log(secondList.length>firstList.length);
+
+// -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// -  Print the sum of the second and the third element
+
+let r=[54,23,66,12];
+console.log(r[1]+r[2])
