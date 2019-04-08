e0d13996fd130f8ce29899dde87c51e918ce4e3b
Soma <soma.feigl@gmail.com>
2019-04-08 18:03:45 +0300

    sum elements

---
+++ week-02/day-1/colors.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+// -  Create a two dimensional list
+//    which can contain the different shades of specified colors
+// -  In `colors[0]` store the shades of green:
+//    `"lime", "forest green", "olive", "pale green", "spring green"`
+// -  In `colors[1]` store the shades of red:
+//    `"orange red", "red", "tomato"`
+// -  In `colors[2]` store the shades of pink:
+//    `"orchid", "violet", "pink", "hot pink"`
+
+let colors = [
+    ["lime", "forest green", "olive", "pale green", "spring green"],
+    ["orange red", "red", "tomato"],
+    ["orchid", "violet", "pink", "hot pink"],
+];
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// -  Log the sum of the elements in `ai` to the console
+
+let ai=[3,4,5,6,7]
+let q:number=0
+for(let i:number=0;i<=ai.length;i++){
+q=ai[i]+q
+}
+console.log(q)
