09ba745a61ed69f6ac5fc8701c6b698c8953b57c
Soma <soma.feigl@gmail.com>
2019-04-09 17:16:08 +0300

    got helped with hashmap

---
+++ week-02/day-2/laststringex.ts
@@ -0,0 +1,24 @@
+'use strict';
+// Things are a little bit messed up
+// Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+// Assemble the fragments into the out variable
+
+let ouet:any = '';
+let notSoCrypticMessage: any[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+
+let hashmap:any = {
+  7: 'run around and desert you',
+  50: 'tell a lie and hurt you ',
+  49: 'make you cry, ',
+  2: 'let you down',
+  12: 'give you up, ',
+  1: 'Never gonna ',
+  11: '\n',
+  3: 'say goodbye '
+};
+
+let idc:any=0;
+for (let i=0; i<notSoCrypticMessage.length;i++){
+    idc=notSoCrypticMessage[i];
+    console.log(hashmap[idc]);
+}
