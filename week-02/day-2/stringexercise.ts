6d1e7e03d2a65201d5f1cb4c3017e074a7abe856
Soma <soma.feigl@gmail.com>
2019-04-09 13:26:50 +0300

    did urlfix

--- week-02/day-2/stringexercise.ts
+++ week-02/day-2/stringexercise.ts
@@ -18,10 +18,21 @@ console.log(examp);
 let reversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
 console.log(reversed);
 
-function reverse(wtf){
+function reverse(wtf) {
     return wtf.split('').reverse().join('');
 }
 
 console.log(reverse(reversed))
-
 console.log(reversed);
+
+
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+let urlll:string= " asdfasdfabotsadsfasdf"
+
+function fixx(idk){
+    return idk.replace("bots","odds").replace("https//","https://")
+}
+console.log(fixx(url));
