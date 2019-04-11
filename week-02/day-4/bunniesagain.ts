1ff1721cc0e54a02b31dba177207564b4cc0840b
Soma <soma.feigl@gmail.com>
2019-04-11 16:07:39 +0300

    feat(bunniesagain) did the first string,NOTETOSELF:cant always use split/splice/join together

--- week-02/day-4/bunniesagain.ts
+++ week-02/day-4/bunniesagain.ts
@@ -16,40 +16,52 @@ console.log(imNotCreativeEnoughForNamingThisManyFunctions(6))
 
 // Given a string, compute recursively (no loops) a new string 
 // where all the lowercase 'x' chars have been changed to 'y' chars.
+let arandomstring: string = "aaaaaaxaaaaaaaaaaxaaaaaaaaaaax";
+let index:number =0;
+let hapci:any="";
+function givenAString(given: string): string {
+    if(given.split("").indexOf("x")===-1){
+        return given
+    }index=given.split("").indexOf("x")
+    hapci=given.split("")
+    hapci.splice(index,1,"y")
+    given=hapci.join("")
+    return givenAString(given)
+  };
+//let orp=arandomstring.split("")
+//  console.log(orp)
+//
+//  console.log(orp.splice(0,1,"y"))
+//
+//  console.log(orp.join(""));
 
-/*function givenAString(given:string):string{
-    if array doesnt have x in it{}
+console.log(givenAString(arandomstring))
 
-this is a string
-i wanna split up the string to arrays
-i wanna change up the x y 
-i wanna jointhem together to a string 
-
-};*/
+function stringF(s:string):string {
+    if (s.length === 1) {
+        if (s === "x") {
+            return "y"
+        }
+            return s
+    }if (s[0] === "x") {
+        return "y".concat(stringF(s.slice(1,s.length))) 
+    }
+    return s[0].concat(stringF(s.slice(1,s.length))) 
+}
+//console.log(stringF("x"))
 
 
 // Write a function that finds the largest element of an array using recursion.
 
-
-/*let osszes:number=myArray.length;
-let xelem:number=0;
-function findsLargestElement(hossz:number):number{
-    if (myArray[xelem]){
-        return myArray[xelem]
-    } return myArray[xelem] if findsLargestElement(hossz)
-};
-console.log(myArray.[])*/
-
-
 let myArray: number[] = [1, 2, 3, 4, 5, 40, 14, 13, 12, 11]
 
-function findsLargestElement(arandomarray: number[]):number{
+function findsLargestElement(arandomarray: number[]): number {
     if (arandomarray.length === 1) {
         return arandomarray[0]
     } else if (arandomarray[0] < arandomarray[1]) {
-        arandomarray.splice(0,1)
+        arandomarray.splice(0, 1)
     } else if (arandomarray[0] > arandomarray[1]) {
-        arandomarray.splice(1,1)
+        arandomarray.splice(1, 1)
     } return findsLargestElement(arandomarray)
 };
 console.log(findsLargestElement(myArray))
