ece082e8def587141aa78e858d46bc082bc6c383
Soma <soma.feigl@gmail.com>
2019-04-12 17:03:18 +0300

    gonna finish at home

---
+++ week-02/day-5/iGaveUp.ts
@@ -0,0 +1,36 @@
+'use strict'
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx: any = canvas.getContext('2d');
+
+
+function drawYesterday(x:number,y:number,num:number) {
+    if(num<=1){
+        return
+    }
+    ctx.beginPath();
+    ctx.strokeStyle = "black"
+    ctx.moveTo(x/3, y/y)
+    ctx.lineTo(x/3, y)
+    ctx.stroke();//first line
+    ctx.beginPath();
+    ctx.strokeStyle = "black"
+    ctx.moveTo(x/1.5, y/y)
+    ctx.lineTo(x/1.5, y)
+    ctx.stroke();//second line
+    ctx.beginPath();
+    ctx.strokeStyle = "black"
+    ctx.moveTo(x/x, y/3)
+    ctx.lineTo(x, y/3)
+    ctx.stroke();//third line
+    ctx.beginPath();
+    ctx.strokeStyle = "black"
+    ctx.moveTo(x/x, y/1.5)
+    ctx.lineTo(x, y/1.5)
+    ctx.stroke();//fourth line
+
+    drawYesterday(x/3,y/3,num-1)
+}
+
+drawYesterday(900,900,5)
+
--- week-02/day-5/lastTryToday.ts
+++ week-02/day-5/lastTryToday.ts
@@ -2,8 +2,11 @@
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx: any = canvas.getContext('2d');
+export{}
+//600,470
 
-function sierpinski(Ax:number,Ay:number,Bx:number,By:number,Cx:number,Cy:number,d:number,ctx:number) {
+
+/* function sierpinski(Ax:number,Ay:number,Bx:number,By:number,Cx:number,Cy:number,d:number,ctx:number) {
     if(d>0) {
         sierpinski(Ax,Ay,(Ax + Cx) / 2,(Ay + Cy) / 2,(Ax + Bx) / 2,(Ay + By) / 2,d-1,ctx);
         sierpinski((Ax + Bx) / 2,(Ay + By) / 2,(Bx + Cx) / 2,(By + Cy) / 2,Bx,By,d-1,ctx);
@@ -22,4 +25,6 @@ ctx.fillStyle = 'white';
 ctx.fill();
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 2;
-ctx.stroke();
+ctx.stroke(); */
+
+
