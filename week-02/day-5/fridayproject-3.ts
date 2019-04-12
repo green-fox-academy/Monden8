741c4007cd7bf5b335da15e85b25aa93f17e420a
Soma <soma.feigl@gmail.com>
2019-04-12 14:29:57 +0300

    finished playing around

--- week-02/day-5/fridayproject-3.ts
+++ week-02/day-5/fridayproject-3.ts
@@ -3,28 +3,32 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx: any = canvas.getContext('2d');
 
-function draw(startX:number, startY:number, len:number, angle:number,branchWidth:number) {
+function draw(startX: number, startY: number, len: number, angle: number, branchWidth: number) {
     ctx.beginPath();
     ctx.save();
+
+    ctx.strokeStyle = "#874312";
     ctx.lineWidth = branchWidth;
     ctx.translate(startX, startY);
-    ctx.rotate(angle * Math.PI/180);
+    ctx.rotate(angle * Math.PI / 180);
     ctx.moveTo(0, 0);
     ctx.lineTo(0, -len);
     ctx.stroke();
-    
-    if(len < 5) {
-      ctx.restore();
-      return;
+
+    if (len < 5) {
+        ctx.restore();
+        ctx.fillStyle = "lawngreen";
+        ctx.arc(0, -len, 2, 0, Math.PI / 2);
+        ctx.fill();
+        return;
     }
-    
-    draw(0, -len, len*0.8, -35,branchWidth*0.75);
-    draw(0, -len, len*0.8, 35,branchWidth*0.75);
-    
+    draw(0, -len, len * 0.8, -30, branchWidth * 0.75);
+    draw(0, -len, len * 0.8, 30, branchWidth * 0.75);
+
     ctx.restore();
-  }
-  
-  
-  draw(400,800,120,0,25);
+}
+
+
+draw(425, 600, 130, 0, 25);
 
 
