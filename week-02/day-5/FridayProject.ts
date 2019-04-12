ac5e60814ed629b053ce953526e830fd487c5803
Soma <soma.feigl@gmail.com>
2019-04-12 12:19:06 +0300

    failed project lets start again

---
+++ week-02/day-5/FridayProject.ts
@@ -0,0 +1,49 @@
+'use strict'
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx: any = canvas.getContext('2d');
+
+//800.800
+
+function drawPyr(elso: number, masodik: number): void {
+    if (elso<=5) {
+    ctx.beginPath();
+    ctx.moveTo(elso, elso);
+    ctx.lineTo(masodik, elso);
+    ctx.lineTo(elso+masodik/ 2, masodik);
+    ctx.lineTo(elso, elso);
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(elso+masodik/ 2, elso);
+    ctx.lineTo(masodik,masodik );
+    ctx.lineTo(elso,masodik );
+    ctx.lineTo(elso+masodik/ 2, elso);
+    ctx.strokeStyle = 'darkpink';
+    ctx.stroke();
+    drawPyr(elso/2,masodik/2)
+    }
+    ctx.beginPath();
+    ctx.moveTo(elso, elso);
+    ctx.lineTo(masodik, elso);
+    ctx.lineTo(elso+masodik/ 2, masodik);
+    ctx.lineTo(elso, elso);
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(elso+masodik/ 2, elso);
+    ctx.lineTo(masodik,masodik );
+    ctx.lineTo(elso,masodik );
+    ctx.lineTo(elso+masodik/ 2, elso);
+    ctx.strokeStyle = 'darkpink';
+    ctx.stroke();
+    drawPyr(elso+1.2,masodik+1.2)
+    
+}
+drawPyr(20, 780);
+
+
+
+
