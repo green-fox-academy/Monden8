3df2374ccf87f9209273454459e49bd87b2e8a67
Soma <soma.feigl@gmail.com>
2019-04-16 16:43:43 +0300

    feat(w-03/d-2)started tic-tac-toe

---
+++ week-03/day-2/tic-tac-toe.js
@@ -0,0 +1,31 @@
+'use strict'
+const fs=require('fs')
+// Write a function that takes a filename as a parameter
+// The file contains an ended Tic-Tac-Toe match
+// We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
+// Return "X", "O" or "Draw" based on the input file
+
+
+function ticTacResult(filename){
+
+
+
+
+    
+if(){
+console.log('O')
+}if(){
+console.log('X')
+}if(){
+console.log('Draw')
+}
+
+console.log(ticTacResult('win-o.txt'))
+// Should print "O"
+
+console.log(ticTacResult('win-x.txt'))
+// Should print "X"
+
+console.log(ticTacResult('draw.txt'))
+// Should print "Draw"
+
