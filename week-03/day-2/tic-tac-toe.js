8b2c878b72d06ea018484f96b7eaf58a66278a24
Soma <soma.feigl@gmail.com>
2019-04-16 17:48:11 +0300

    started tictactoe-my brain died

--- week-03/day-2/tic-tac-toe.js
+++ week-03/day-2/tic-tac-toe.js
@@ -1,25 +1,33 @@
 'use strict'
-const fs=require('fs')
+const fs=require('fs');
 // Write a function that takes a filename as a parameter
 // The file contains an ended Tic-Tac-Toe match
 // We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
 // Return "X", "O" or "Draw" based on the input file
-
+let indexesOFfirstO=[];
 
 function ticTacResult(filename){
+let readfile=fs.readFileSync(filename,'utf8');
+let banana=readfile.split('\n');
+for(let i=0;i<banana.length;i++){
+indexesOFfirstO.push((banana[i].indexOf('O')))}
+console.log(indexesOFfirstO)
+};
 
+ticTacResult('win-o.txt')
 
-
-
-    
-if(){
+/*
+if(indexesOFfirstO=0,0,0)||{
 console.log('O')
 }if(){
 console.log('X')
 }if(){
 console.log('Draw')
 }
+*/
 
+
+/*
 console.log(ticTacResult('win-o.txt'))
 // Should print "O"
 
@@ -28,4 +36,4 @@ console.log(ticTacResult('win-x.txt'))
 
 console.log(ticTacResult('draw.txt'))
 // Should print "Draw"
-
+*/
