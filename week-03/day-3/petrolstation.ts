57cd16dcd4acda0f39ec5eb8a0b94902c27400b0
Soma <soma.feigl@gmail.com>
2019-04-17 19:49:55 +0200

    started petrol

---
+++ week-03/day-3/petrolstation.ts
@@ -0,0 +1,28 @@
+'use strict'
+
+//  Create a Station and a Car classes
+//  Station
+//  gasAmount
+//  refill(car) -> decreases the gasAmount by the capacity of the car and increases the cars gasAmount
+//  Car
+//  gasAmount
+//  capacity
+//  create constructor for Car where:
+//  initialize gasAmount -> 0
+//  initialize capacity -> 100
+
+class Station {
+    gasAmount: number;
+    refill(car) {
+        this.gasAmount -= car.capacity && car.gasAmount +=this.gasAmount
+    }
+}
+class Car {
+    gasAmount: number;
+    capacity:number;
+
+    constructor(){
+        this.gasAmount=0;
+        this.capacity=100;
+    }
+}
