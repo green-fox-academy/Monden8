0d76c9e897e698d1764415e1ca3caed9861e4e7e
Soma <soma.feigl@gmail.com>
2019-04-18 09:54:32 +0200

    ŐemFau

--- week-03/day-3/petrolstation.ts
+++ week-03/day-3/petrolstation.ts
@@ -12,17 +12,30 @@
 //  initialize capacity -> 100
 
 class Station {
-    gasAmount: number;
-    refill(car) {
-        this.gasAmount -= car.capacity && car.gasAmount +=this.gasAmount
+    gasAmount: number = 1000;
+
+    constructor() {
+        this.gasAmount;
+    }
+
+    refill(car: Car) {
+        this.gasAmount -= car.capacity ;
+        car.gasAmount += car.capacity
+       // car.capacity=0;
     }
 }
 class Car {
     gasAmount: number;
-    capacity:number;
+    capacity: number;
 
-    constructor(){
-        this.gasAmount=0;
-        this.capacity=100;
+    constructor() {
+        this.gasAmount = 0;
+        this.capacity = 100;
     }
 }
+
+let mazda6 : Car = new Car;
+let omw : Station = new Station;
+omw.refill(mazda6)
+console.log(omw)
+console.log(mazda6)
