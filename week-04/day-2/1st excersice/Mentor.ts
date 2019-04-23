20680d4aad5f04b63102a68f22d262a92f91c76d
Soma <soma.feigl@gmail.com>
2019-04-23 11:21:00 +0200

    feat(day-2/person.ts) made the base

---
+++ week-04/day-2/1st excersice/Mentor.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+//   Create a Mentor class that has the same fields and methods as the Person class, and has the following additional
+//   
+//   fields:
+//   level: the level of the mentor (junior / intermediate / senior)
+//   methods:
+//   getGoal(): prints out 'My goal is: Educate brilliant junior software developers.'
+//   introduce(): 'Hi, I'm name, a age year old gender level mentor.'
+//   The Mentor class has the following constructors:
+//   
+//   Mentor(name, age, gender, level)
+//   Mentor(): sets name to Jane Doe, age to 30, gender to female, level to intermediate
