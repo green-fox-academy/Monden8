ad9d692b8869fc6babc795569a5353ad67d16ec6
Soma <soma.feigl@gmail.com>
2019-04-23 17:34:17 +0200

    feat(3rd excersice/aircraft.ts)finished carrier

--- week-04/day-2/3rd excersice/carrier.ts
+++ week-04/day-2/3rd excersice/carrier.ts
@@ -1,11 +1,3 @@
-// if (this.lotOfAircrafts.forEach(element => { element.isPriority() == true })){
-//     this.lotOfAircrafts.forEach(element => { element.refill(this.supplybox) })
-// } else {
-//     this.lotOfAircrafts.forEach(element => { element.refill(this.supplybox) })
-// } return this.supplybox
-
-// this.lotOfAircrafts.forEach(element => {
-//   element.isPriority() ? element.refill(this.supplybox) : continue
 'use strict';
 import { Aircrafts } from './aircraft'
 import { F16 } from './F16'
@@ -47,18 +39,15 @@ export class Carrier {
         } return this.totalDMGdealt
     }
     getStatus() {
-        console.log(` HP: ${this.healthPoint}, Aircraft count: ${this.lotOfAircrafts.length}, Ammo Storage: ${this.supplyboxOfCarrier}, Total damage: ${this.totalDMGdealt}`)
-        console.log('Aircrafts:')
-        for(let i: number = 0; i < this.lotOfAircrafts.length; i++){
-            console.log()
+        if (this.healthPoint > 0) {
+            console.log(`HP: ${this.healthPoint}, Aircraft count: ${this.lotOfAircrafts.length}, Ammo Storage: ${this.supplyboxOfCarrier}, Total damage: ${this.totalDMGdealt}`)
+            console.log('Aircrafts:')
+            for (let i: number = 0; i < this.lotOfAircrafts.length; i++) {
+                console.log(`Type ${this.lotOfAircrafts[i].type}, Ammo: ${this.lotOfAircrafts[i].ammunition},Base Damage: ${this.lotOfAircrafts[i].baseDamage}, All Damage: ${this.lotOfAircrafts[i].fight()}`)
+            }
+        } else if (this.healthPoint <= 0) {
+            console.log('It\'s dead Jim :(')
         }
-    //   Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
-    //   Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
-    //   Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
-    //   Type F16, Ammo: 8, Base Damage: 30, All Damage: 240
-    //   Type F16, Ammo: 8, Base Damage: 30, All Damage: 240
-
-        //   If the health points are 0 then it should return: It's dead Jim :(
     }
 };
 let anyahajo: Carrier = new Carrier()
@@ -66,18 +55,6 @@ let babahajo: Carrier = new Carrier()
 let jano: Aircrafts = new F16()
 anyahajo.add(jano)
 let pisti: Aircrafts = new F35()
-babahajo.add(pisti)
+anyahajo.add(pisti)
 anyahajo.fill()
-babahajo.fill()
-//   Methods:
-//   getStatus
-//   It should return a string about itself and all of its aircrafts' statuses in the following format:
-//   
-//   HP: 5000, Aircraft count: 5, Ammo Storage: 2300, Total damage: 2280
-//   Aircrafts:
-//   Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
-//   Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
-//   Type F35, Ammo: 12, Base Damage: 50, All Damage: 600
-//   Type F16, Ammo: 8, Base Damage: 30, All Damage: 240
-//   Type F16, Ammo: 8, Base Damage: 30, All Damage: 240
-//   If the health points are 0 then it should return: It's dead Jim :(
+anyahajo.getStatus()
