bc3c1c97b02df039b7216d40e91ea47b77b936f5
Soma <soma.feigl@gmail.com>
2019-04-23 12:17:15 +0200

    feat (day-2/Cohort.ts) working

--- week-04/day-2/1st excersice/Cohort.ts
+++ week-04/day-2/1st excersice/Cohort.ts
@@ -1,6 +1,8 @@
 'use strict';
 import { Person } from './Person'
 import { Mentor } from './Mentor'
+import { Student } from './Student'
+import { Sponsor } from './Sponsor'
 
 //    Create a Cohort class that has the following
 //    
@@ -37,3 +39,52 @@ class Cohort {
             console.log(`The name cohort has ${this.cStudents.length} students and ${this.cMentors.length} mentors.`)
         }
     }
+    
+    
+/*    let people = [];
+
+let mark = new Person('Mark', 46, 'male');
+people.push(mark);
+
+let jane = new Person();
+people.push(jane);
+
+let john = new Student('John Doe', 20, 'male', 'BME');
+people.push(john);
+
+let student = new Student();
+people.push(student);
+
+let gandhi = new Mentor('Gandhi', 148, 'male', 'senior');
+people.push(gandhi);
+
+let mentor = new Mentor();
+people.push(mentor);
+
+let elon = new Sponsor('Elon Musk', 46, 'male', 'SpaceX');
+people.push(elon);
+
+let sponsor = new Sponsor();
+people.push(sponsor);
+
+student.skipDays(3);
+
+for (let i = 0; i < 6; i++) {
+  elon.hire();
+}
+
+for (let i = 0; i < 4; i++) {
+  sponsor.hire();
+}
+
+for (let person of people) {
+  person.introduce();
+  person.getGoal();
+}
+
+let awesome = new Cohort('AWESOME');
+awesome.addStudent(student);
+awesome.addStudent(john);
+awesome.addMentor(mentor);
+awesome.addMentor(gandhi);
+awesome.info();  */
