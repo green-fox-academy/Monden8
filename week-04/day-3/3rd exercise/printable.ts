b237c6528670b20ba833f8b9cc28aad5c8ed44d4
Soma <soma.feigl@gmail.com>
2019-04-24 16:08:54 +0200

    feat(day-3/3rd and 1st ex) made printable

---
+++ week-04/day-3/3rd exercise/printable.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+//   Create a new interface called Printable
+//   it should have one method definition called printAllFields
+//   implement this interface on the Domino
+
+export interface Printable{
+    printAllFields():void
+}
