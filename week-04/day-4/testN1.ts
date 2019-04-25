7ad30d6af3508d7c5be07ba3c7294fbaa2ffdcab
Soma <soma.feigl@gmail.com>
2019-04-25 11:09:28 +0200

    feat(day-4/testN1.ts) added apple class and tested the output succesfully

---
+++ week-04/day-4/testN1.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import { Test } from 'tape';
+var test = require('tape');
+
+class Apple {
+    getApple() {
+        return 'apple'
+    };
+};
+
+let alma: Apple = new Apple();
+
+test('should return apple', function (t: any) {
+    let nindzsa7 = alma.getApple();
+    t.equal(nindzsa7, 'apple');
+    t.end();
+});
