2e0fede4bbb1635775602f81b7938ce864db6578
Soma <soma.feigl@gmail.com>
2019-04-26 12:50:55 +0200

    feat(Most Common)added if's & some comments since not perfect but will do

--- week-04/TrialExam/Most Common Characters/file.IO.ts
+++ week-04/TrialExam/Most Common Characters/file.IO.ts
@@ -1,11 +1,6 @@
 'use strict';
 declare function require(fs: string): any
 let fis = require('fs')
-//  Write a function that takes a filename as a parameter.
-//  Return the 2 most common characters and their occurrences in the file's content.
-//  
-//  If the file does not exist throw an exception with the following message:
-//  "File does not exist!"
 
 let mostCommons = function (file: string) {
     //  try {
@@ -19,30 +14,43 @@ let mostCommons = function (file: string) {
     let stringArray: string[] = [];
     stringArray = redFile.split('');
     let stringObject: any = {};
-    let abc: string;
+    let abc: any = {};
     let hossz: number = stringArray.length;
     for (let i = 0; i < hossz; i++) {
         if (Object.keys(stringObject).includes(stringArray[i])) {
             stringObject[stringArray[i]] += 1;
-        } else { stringObject[stringArray[i]] = 1; }
-    }
+        } else {
+            stringObject[stringArray[i]] = 1;
+        }
+    };
     // for (let j: number = 1; j >= 0; j--) {
     for (let i: number = 0; i < hossz; i++) {
         if (Object.values(stringObject)[i] > Object.values(stringObject)[i + 1]) {
             if (Object.values(stringObject)[i] > Object.values(stringObject)[i + 1] < Object.values(stringObject)[i + 2]) {
                 //if (j == 1) {
-                console.log(`"${Object.keys(stringObject)[i]}" : ${Object.values(stringObject)[i]} `)
-                //} else {
-                // console.log(`"${Object.keys(stringObject)[i]}" : ${Object.values(stringObject)[i]} `)
-                // }
-                //}
-            }
+                console.log(`"${Object.keys(stringObject)[i]}" : ${Object.values(stringObject)[i]} `);
+            };
+            //} else {
+            // console.log(`"${Object.keys(stringObject)[i]}" : ${Object.values(stringObject)[i]} `)
+            // }
+            //}
         }
     }
-}
+};
 try {
-    mostCommons('countchaasdr.txt')
+    mostCommons('countchar.txt');
 
 } catch (e) {
-    console.log(`There isnt such file`)
+    console.log(`File does not exists!`);
+};
+/*
+let toSort = [];
+    for (var numbers in stringObject) {
+        sortable.push([numbers, stringObject[numbers]]);
+    }
+
+    toSort.sort(function (a, b) {
+        return a[1] - b[1];
+    });
 }
+*/
