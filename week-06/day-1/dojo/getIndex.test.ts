fe436941c7a5951809720e4165f96697af541639
Soma <soma.feigl@gmail.com>
2019-05-13 14:55:43 +0200

    refactored

--- week-06/day-1/dojo/getIndex.test.ts
+++ week-06/day-1/dojo/getIndex.test.ts
@@ -2,17 +2,21 @@
 
 let test = require('tape');
 declare function require(test: any): any;
-import { getIndex } from './getIndex';
-
-
+import { getIndex } from './getIndex'
 test('Correct solution', (t: any) => {
     const testArray: string[] = ['alma', 'banan', 'citrom', 'dinnye', 'eper'];
-    t.equal(getIndex(testArray, 'alma'), 0);
+    t.deepEqual(getIndex(testArray, 'alma'), [0]);
     t.end();
 }),
 
     test('Correct solution', (t: any) => {
         const testArray: string[] = ['alma', 'banan', 'citrom', 'dinnye', 'eper'];
-        t.equal(getIndex(testArray, 'afonya'), -1);
+        t.deepEqual(getIndex(testArray, 'afonya'), [-1]);
         t.end();
     })
+
+test('Correct solution', (t: any) => {
+    const testArray: string[] = ['alma', 'banan', 'citrom', 'dinnye', 'eper', 'alma'];
+    t.deepEqual(getIndex(testArray, 'alma'), [0, 5]);
+    t.end();
+})
--- week-06/day-1/dojo/getIndex.ts
+++ week-06/day-1/dojo/getIndex.ts
@@ -1,10 +1,14 @@
 'use strict';
 
-let getIndex = function (array: string[], value: string): number {
+export let getIndex = function (array: string[], value: string): number[] {
+    let multipleIndexArray: number[] = [];
     for (let i: number = 0; i < array.length; i++) {
         if (array[i] == value) {
-            return i
-        };
-    }; return -1
+            multipleIndexArray.push(i)
+        }
+    }; if (multipleIndexArray.length > 0) {
+        return multipleIndexArray
+    } else {
+        return [-1]
+    }
 };
-export { getIndex };
