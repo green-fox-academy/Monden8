7e43430b976b9d71f59801c4aaaf988d8bb2b060
Soma <soma.feigl@gmail.com>
2019-05-13 15:27:16 +0200

    chore pulled in variables into test

--- week-06/day-1/dojo/annagram.test.ts
+++ week-06/day-1/dojo/annagram.test.ts
@@ -3,14 +3,15 @@
 let test = require('tape');
 declare function require(test: any): any;
 import { Anagramma } from './Anagram'
-let YourInputString: string = 'uigchighcihg';
-let MyInputString: string = 'almAAA';
-let NindzsaharcosInputstring: string = 'LaAmAA'
 test('Should throw true', function (t: any) {
+    let MyInputString: string = 'almAAA';
+    let NindzsaharcosInputstring: string = 'LaAmAA';
     t.equal(Anagramma(MyInputString, NindzsaharcosInputstring), true);
     t.end();
 });
 test('Should throw false', function (t: any) {
+    let YourInputString: string = 'uigchighcihg';
+    let NindzsaharcosInputstring: string = 'LaAmAA';
     t.equal(Anagramma(YourInputString, NindzsaharcosInputstring), false);
     t.end();
 });
