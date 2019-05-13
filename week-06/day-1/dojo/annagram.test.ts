a9dffe34a56da040ee5a7d82872954f93e26161e
Soma <soma.feigl@gmail.com>
2019-05-13 15:28:26 +0200

    chore renamed function

--- week-06/day-1/dojo/annagram.test.ts
+++ week-06/day-1/dojo/annagram.test.ts
@@ -2,16 +2,16 @@
 
 let test = require('tape');
 declare function require(test: any): any;
-import { Anagramma } from './Anagram'
+import { anagram } from './Anagram'
 test('Should throw true', function (t: any) {
     let MyInputString: string = 'almAAA';
     let NindzsaharcosInputstring: string = 'LaAmAA';
-    t.equal(Anagramma(MyInputString, NindzsaharcosInputstring), true);
+    t.equal(anagram(MyInputString, NindzsaharcosInputstring), true);
     t.end();
 });
 test('Should throw false', function (t: any) {
     let YourInputString: string = 'uigchighcihg';
     let NindzsaharcosInputstring: string = 'LaAmAA';
-    t.equal(Anagramma(YourInputString, NindzsaharcosInputstring), false);
+    t.equal(anagram(YourInputString, NindzsaharcosInputstring), false);
     t.end();
 });
