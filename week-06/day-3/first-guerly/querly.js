82013d3eb372e4be695f1cbe3129f8bfa20c22b3
Soma <soma.feigl@gmail.com>
2019-05-15 11:37:13 +0200

    feat(first-querly) made the base for the excersice

---
+++ week-06/day-3/first-guerly/querly.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const king = document.getElementById('b325');
+console.log(king);
+const man = document.getElementsByTagName('p');
+const lighter = document.getElementsByClassName('asteroid b329 big');
+console.log(man + lighter);
+const conceitedKing = document.getElementsByTagName('section');
+window.alert(conceitedKing[0]);
+window.alert(conceitedKing[1]);
+const noBusiness = document.getElementById('div')
+for(let i =0 ; i<3 ;i++){
+console.log(noBusiness[i]);
+};
