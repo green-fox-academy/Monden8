22e48a04ef8e507131be257834b30306d71e99cf
Soma <soma.feigl@gmail.com>
2019-05-15 11:49:30 +0200

    feat(first-querly) refactored the excersice so its actually working

--- week-06/day-3/first-guerly/querly.js
+++ week-06/day-3/first-guerly/querly.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const king = document.getElementById('b325');
-console.log(king);
-const man = document.getElementsByTagName('p');
-const lighter = document.getElementsByClassName('asteroid b329 big');
-console.log(man + lighter);
-const conceitedKing = document.getElementsByTagName('section');
-window.alert(conceitedKing[0]);
-window.alert(conceitedKing[1]);
-const noBusiness = document.getElementById('div')
-for(let i =0 ; i<3 ;i++){
-console.log(noBusiness[i]);
+console.log(king.innerText);
+const manLighter = document.querySelectorAll('.big');
+console.log(manLighter[0].innerText);
+console.log(manLighter[1].innerText);
+const conceitedKing = document.querySelector('section').querySelectorAll('div');
+window.alert(conceitedKing[0].innerText);
+window.alert(conceitedKing[1].innerText);
+const noBusiness = document.querySelectorAll('div')
+for(let i =0 ; i<2 ;i++){
+console.log(noBusiness[i].innerText);
 };
+document.querySelector('body').setAttribute('style','opacity:0')
