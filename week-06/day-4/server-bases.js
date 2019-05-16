8c09d3012f476c9ff08dcd3a152d535063b7f965
Soma <soma.feigl@gmail.com>
2019-05-16 12:17:55 +0200

    finished server-side basics first

---
+++ week-06/day-4/server-bases.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//express.static('./assets/result.png', [])
+//app.use(express.static('public'));
