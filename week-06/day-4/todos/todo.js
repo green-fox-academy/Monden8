9de4f8878179a9ea62f48e91af773cf6f7cb9e1c
Soma <soma.feigl@gmail.com>
2019-05-16 13:59:01 +0200

    feat (todo) moved for loop logic to ejs template

--- week-06/day-4/todos/todo.js
+++ week-06/day-4/todos/todo.js
@@ -8,24 +8,14 @@ const todos = [
 ];
 
 app.set('view engine', 'ejs');
-for (let i = 0; i < todos.length; i++) {
-    content = todos[i];
-
-    app.get('/', (req, res) => {
-        res.render('home', {
-            title: 'List to Do',
-            content: ''
-            /*
-            let element = document.createElement('ul')
-            element.innerHTML = `<li></li>
-             ${Array(5).join(0).split(0).map((item, i) => `
-              <div>I am item number ${i}.</div>`).join('')}`
-            */
-        }
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'List to Do',
+        lista: todos
+    });
 });
 
-
-    // start express app on port 3000
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
-    });
+// start express app on port 3000
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
