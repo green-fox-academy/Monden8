1a466a39958cb461fc2396a54cd78aa6fed35504
Soma <soma.feigl@gmail.com>
2019-05-17 17:09:37 +0200

    made mini pictures and the function for changing them

--- week-06/day-5/slideshow.js
+++ week-06/day-5/slideshow.js
@@ -1,37 +1,41 @@
 //adding main image
-let images = ['./cave.jpg', './abandoned.jpg', './fish.jpg']
+let images = ['file:///home/soma/Documents/Monden8/week-06/day-5/cave.jpg', 'file:///home/soma/Documents/Monden8/week-06/day-5/abandoned.jpg', 'file:///home/soma/Documents/Monden8/week-06/day-5/fish.jpg']
 let firstPic = document.querySelector('img');
 firstPic.src = images[0];
-let i=0;
-function lefti(){
-    if(i>0){
-    firstPic.src=images[i-1]
-    i--
-    }return i
-};
-function righti(){
-    if(i<2){
-    firstPic.src=images[i+1]
-    i++
+let i = 0;
+function lefti() {
+    if (i > 0) {
+        i--
+        firstPic.src = images[i]
+    } else {
+        i = images.length - 1
+        firstPic.src = images[i]
     }
-    return i
-};  
-// function for button left  //if button innexrtext--< i-1
-// function for button right //if button innexrtext--> i+1
-//adding images with foorloop // data-id=6
-
-/*
-for (let i = 0; i < kids.length; i++) {
-    let list = document.createElement('article');
-    let h3owner = document.createElement('h3');
-    let ppetname = document.createElement('p');
-    let petName = (kids[i].petName);
-    let owner = (kids[i].owner)
-    h3owner.appendChild(document.createTextNode(owner));
-    ppetname.appendChild(document.createTextNode(petName));
-    list.appendChild(h3owner);
-    list.appendChild(ppetname);
-    document.querySelector('div').appendChild(list);};
-*/
-
+};
+function righti() {
+    if (i < images.length - 1) {
+        i++
+        firstPic.src = images[i]
+    } else {
+        i = 0
+        firstPic.src = images[i]
+    }
+};
 // function for images // change main image i--> innertext of image.src
+function change(pic) {
+    firstPic.src = pic.src;
+    i = images.indexOf(pic.src);
+};
+//adding images with foorloop // data-id=3
+let lastDiv = document.querySelectorAll('div');
+images.forEach(element => {
+    let miniPic = document.createElement('img');
+    miniPic.setAttribute('width', '10px');
+    miniPic.setAttribute('height', '10px');
+    miniPic.classList.add(element);
+    miniPic.addEventListener("click", function (event) {
+        change(miniPic)
+    })
+    miniPic.src = element;
+    lastDiv[2].append(miniPic)
+});
