2afac8ed3cf5c4067d1e89bf6229fe110e0e1887
Soma <soma.feigl@gmail.com>
2019-05-17 17:39:39 +0200

    made array useful again

--- week-06/day-5/slideshow.js
+++ week-06/day-5/slideshow.js
@@ -1,41 +1,42 @@
 //adding main image
-let images = ['file:///home/soma/Documents/Monden8/week-06/day-5/cave.jpg', 'file:///home/soma/Documents/Monden8/week-06/day-5/abandoned.jpg', 'file:///home/soma/Documents/Monden8/week-06/day-5/fish.jpg']
+let images = ['/cave.jpg', '/abandoned.jpg', '/fish.jpg']
 let firstPic = document.querySelector('img');
-firstPic.src = images[0];
+let currentMap ='file:///home/soma/Documents/Monden8/week-06/day-5';
+firstPic.src = currentMap + images[0];
 let i = 0;
 function lefti() {
     if (i > 0) {
         i--
-        firstPic.src = images[i]
+        firstPic.src = currentMap + images[i]
     } else {
         i = images.length - 1
-        firstPic.src = images[i]
+        firstPic.src = currentMap + images[i]
     }
 };
 function righti() {
     if (i < images.length - 1) {
         i++
-        firstPic.src = images[i]
+        firstPic.src = currentMap + images[i]
     } else {
         i = 0
-        firstPic.src = images[i]
+        firstPic.src = currentMap + images[i]
     }
 };
 // function for images // change main image i--> innertext of image.src
 function change(pic) {
     firstPic.src = pic.src;
-    i = images.indexOf(pic.src);
+    i = images.indexOf(pic.classList[0]);
 };
-//adding images with foorloop // data-id=3
+//adding images with foorloop
 let lastDiv = document.querySelectorAll('div');
 images.forEach(element => {
     let miniPic = document.createElement('img');
     miniPic.setAttribute('width', '10px');
     miniPic.setAttribute('height', '10px');
     miniPic.classList.add(element);
+    miniPic.src = currentMap + element;
     miniPic.addEventListener("click", function (event) {
         change(miniPic)
     })
-    miniPic.src = element;
     lastDiv[2].append(miniPic)
 });
