e9b587a2417bca092aa82e872e3545f9615db78b
Soma <soma.feigl@gmail.com>
2019-05-21 12:01:09 +0200

    feat callback-1.js understod why we use callback

--- week-07/day-2/callbacks/callback-1.js
+++ week-07/day-2/callbacks/callback-1.js
@@ -1,26 +1,32 @@
 'use strict';
 
 const mapWith = (input, callback) => {
-  let output = [];
-  for (let i = 0; i < input.length; i++) {
-     output[i]=callback(input[i]);
-  };
-  return output;
+    let output = [];
+    for (let i = 0; i < input.length; i++) {
+        output.push(callback(input[i]));
+    };
+    return output;
 };
 
 const addOne = (number) => {
-  return number + 1;
-}
-
+    return number + 1;
+};
 // Exercise 1:
 
 console.log(mapWith([1, 2, 3], addOne));
 
 // Exercise 2:
-
-// Create a callback function which removes every second character from a string
-
 const words = ['map', 'reduce', 'filter'];
 
-//console.log(mapWith(words, removeSecondLetter));
+let removesecondLetter = (data) => {
+    let counter = data.length;
+    let comeback=[];
+    for (let i = 0; i < counter; i+=2) {
+        comeback.push(data.split('').splice(i, 1));  
+    };
+    return comeback.join('');
+};
+
+console.log(mapWith(words, removesecondLetter));
+
 // expected result: ['mp','rdc', 'fle']
