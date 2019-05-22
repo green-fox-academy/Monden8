4db2853048b999b89df7510aae91be631214c944
Soma <soma.feigl@gmail.com>
2019-05-22 16:31:04 +0200

    feat (array_handler) made the base

---
+++ week-07/day-3/array_handler/app.js
@@ -0,0 +1,21 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use('/assets', express.static('assets'));
+app.set('port', 3000);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.listen(3000, function () {
+  console.log(`Yo, yo, yo my n*nja I'm listenin' on port 3000`);
+});
+
+app.get('/arrays', (req,res)=>{
+    app.send({
+
+    })
+});
