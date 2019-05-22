3915d83e5f389e1863b15588be0cc8b1cb076be7
Soma <soma.feigl@gmail.com>
2019-05-22 17:06:04 +0200

    completed the task used app.use

--- week-07/day-3/array_handler/app.js
+++ week-07/day-3/array_handler/app.js
@@ -1,21 +1,41 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use('/assets', express.static('assets'));
 app.set('port', 3000);
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+
+app.post('/arrays', (req, res) => {
+    let whatTo = req.body.what
+    let numberIT = req.body.numbers
+    let result = 0;
+    let resultArray = [];
+    if (whatTo === "sum") {
+        for (let i = 0; i < numberIT.length; i++) {
+            result += numberIT[i]
+        }
+    } else if (whatTo === "multiply") {
+        result = 1;
+        for (let i = 0; i < numberIT.length; i++) {
+            result *= numberIT[i]
+        }
+    } else if (whatTo === "double") {
+        for (let i = 0; i < numberIT.length; i++) {
+            resultArray.push(numberIT[i] * 2)
+        }
+    }
+    if (result == 0) {
+        res.send({
+            resultArray
+        })
+    } else {
+        res.send({
+            result
+        })
+    }
 });
+
 app.listen(3000, function () {
-  console.log(`Yo, yo, yo my n*nja I'm listenin' on port 3000`);
-});
-
-app.get('/arrays', (req,res)=>{
-    app.send({
-
-    })
+    console.log(`Yo, yo my n*nja I'm listenin' on port 3000`);
 });
