626aea2fd5d5c5cff118583abde367edfb82125a
Soma <soma.feigl@gmail.com>
2019-05-22 17:35:15 +0200

    feat(yodaaaa/app.js) strugüling with string modification

--- week-07/day-3/yodaaaaa/app.js
+++ week-07/day-3/yodaaaaa/app.js
@@ -6,4 +6,32 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.set('port', 3000);
 
-app.post('/arrays', (req, res) => {})
+app.post('/sith', (req, res) => {
+    let sentence = req.body.text;
+    let stringArray = [];
+    let sithSentence = '';
+    let randomArray = ['Err', 'Arrgh', 'Uhmm'];
+    if (sentence !== undefined) {
+        stringArray = sentence.split(' ')
+        //if(stringArray.length%2===0){
+        for (let i = 1; i < stringArray.length; i += 2) {
+            sithSentence = sithSentence.concat(' ', stringArray[i])
+            i--
+            sithSentence = sithSentence.concat(' ', stringArray[i])
+            i++
+        }
+        //}else {
+
+        //}        
+        res.send({
+            "sith_text": sithSentence
+        })
+    } else {
+        res.send({
+            "error": "Feed me some text you have to, padawan young you are. Hmmm."
+        })
+    }
+});
+app.listen(3000, function () {
+    console.log(`Yo my n*nja I'm listenin' on port 3000`);
+});
