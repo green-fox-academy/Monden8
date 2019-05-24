37a554df2bf475576b3ba09edd8712724fb1cc37
Soma <soma.feigl@gmail.com>
2019-05-24 17:15:43 +0200

    refactored with ?;[data] put delete con.q into con.q added .send to error case statuses

--- week-07/day-5/server.js
+++ week-07/day-5/server.js
@@ -26,7 +26,7 @@ app.get('/posts', (req, res) => {
     con.query(`SELECT * FROM Reddit_Backend;`, (err, rows) => {
         if (err) {
             console.log(err.toString());
-            res.status(500);
+            res.status(500).send();
             return;
         }
         console.log('Dowloaded Matrix');
@@ -36,17 +36,17 @@ app.get('/posts', (req, res) => {
 
 app.post('/posts', (req, res) => {
     con.query(`INSERT INTO Reddit_Backend(title,url) 
-    VALUES("${req.body.title}","${req.body.url}");`, (err) => {
+    VALUES(?,?);`,[req.body.title,req.body.url], (err) => {
             if (err) {
                 console.log(err.toString());
-                res.status(500);
+                res.status(500).send();
                 return;
             }
             console.log('Updated Matrix');
             con.query(`SELECT * FROM Reddit_Backend ORDER BY id DESC LIMIT 1;`, (err, rows) => {
                 if (err) {
                     console.log(err.toString());
-                    res.status(500);
+                    res.status(500).send();
                     return;
                 }
                 res.status(200).json(rows);
@@ -56,19 +56,19 @@ app.post('/posts', (req, res) => {
 
 app.put('/posts/:id', (req, res) => {
     con.query(`UPDATE Reddit_Backend
-        SET title = "${req.body.title}"
-        WHERE id = ${req.params.id};`, (err) => {
+        SET title = ?,
+        WHERE id = ?;`[req.body.title,req.params.id], (err) => {
             if (err) {
                 console.log(err.toString());
-                res.status(500);
+                res.status(500).send();
                 return;
             }
             console.log('Modified Matrix');
             con.query(`SELECT * FROM Reddit_Backend 
-            WHERE id = ${req.params.id};`, (err, rows) => {
+            WHERE id = ?;`[req.params.id], (err, rows) => {
                     if (err) {
                         console.log(err.toString());
-                        res.status(500);
+                        res.status(500).send();
                         return;
                     }
                     res.status(200).json(rows);
@@ -79,22 +79,22 @@ app.put('/posts/:id', (req, res) => {
 app.delete('/posts/:id', (req, res) => {
     let deletedData;
     con.query(`SELECT * FROM Reddit_Backend 
-            WHERE id = ${req.params.id};`, (err, rows) => {
+            WHERE id = ?;`[req.params.id], (err, rows) => {
             if (err) {
                 console.log(err.toString());
-                res.status(500);
+                res.status(500).send();
                 return;
             }
             deletedData = rows;
-        });
-    con.query(`DELETE FROM Reddit_Backend
-            WHERE id=${req.params.id};`, (err) => {
-            if (err) {
-                console.log(err.toString());
-                res.status(500);
-                return;
-            }
-            res.status(200).json(deletedData);
+            con.query(`DELETE FROM Reddit_Backend
+            WHERE id = ?;`[req.params.id], (err) => {
+                    if (err) {
+                        console.log(err.toString());
+                        res.status(500).send();
+                        return;
+                    }
+                    res.status(200).json(deletedData);
+                });
         });
 });
 
@@ -103,7 +103,7 @@ let doesUPcontain;
 (con.query(`SELECT up_posts FROM Logged_User`, (err, rows) => {
     if (err) {
         console.log(err.toString());
-        res.status(500);
+        res.status(500).send();
         return;
     } doesUPcontain = rows;
 }));
@@ -111,7 +111,7 @@ let doesDOWNcontain;
 (con.query(`SELECT down_posts FROM Logged_User`, (err, rows) => {
     if (err) {
         console.log(err.toString());
-        res.status(500);
+        res.status(500).send();
         return;
     } doesDOWNcontain = rows;
 }));
@@ -119,25 +119,25 @@ let postDOTid;
 (con.query(`SELECT id FROM Reddit_Backend`, (err, rows) => {
     if (err) {
         console.log(err.toString());
-        res.status(500);
+        res.status(500).send();
         return;
     } postDOTid = rows;
 }));
 
 //counting variables
 let upVotes;
-(con.query(`SELECT upname_id FROM Reddit_Backend WHERE id = `, (err, rows) => {
+(con.query(`SELECT upname_id FROM Reddit_Backend WHERE id = `/*FUNCTION*/, (err, rows) => {
     if (err) {
         console.log(err.toString());
-        res.status(500);
+        res.status(500).send();
         return;
     } upVotes = rows.split(',').length;
 }));
 let downVotes;
-(con.query(`SELECT downName_id FROM Reddit_Backend WHERE id = `, (err, rows) => {
+(con.query(`SELECT downName_id FROM Reddit_Backend WHERE id = `/*FUNCTION*/, (err, rows) => {
     if (err) {
         console.log(err.toString());
-        res.status(500);
+        res.status(500).send();
         return;
     } downVotes = rows.split(',').length;
 }));
@@ -147,10 +147,10 @@ app.post('/posts/:id/upvote', (req, res) => {
     if (doesUPcontain !== postDOTid) {
         con.query(`UPDATE Reddit_Backend
         SET score = "${upVotes - downVotes}"
-        WHERE id = ${req.params.id};`, (err) => {
+        WHERE id = ?;`[req.params.id], (err) => {
                 if (err) {
                     console.log(err.toString());
-                    res.status(500);
+                    res.status(500).send();
                     return;
                 } else {
                     res.status.status(200).json();
@@ -166,10 +166,10 @@ app.post('/posts/:id/downvote', (req, res) => {
     if (doesDOWNcontain !== postDOTid) {
         con.query(`UPDATE Reddit_Backend
         SET score = "${upVotes - downVotes}"
-        WHERE id = ${req.params.id};`, (err) => {
+        WHERE id = ?;`[req.params.id], (err) => {
                 if (err) {
                     console.log(err.toString());
-                    res.status(500);
+                    res.status(500).send();
                     return;
                 } else {
                     res.status.status(200).json();
@@ -180,15 +180,6 @@ app.post('/posts/:id/downvote', (req, res) => {
     console.log('User already upvoted this post');
     res.status(405).json();
 });
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Yo my n*gga I'm listenin' on ${PORT}. Wazzup dog ?`);
