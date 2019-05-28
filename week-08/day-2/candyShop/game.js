9ddbe231f66ea621f30e47dde6fa3bd69b9dc67c
Soma <soma.feigl@gmail.com>
2019-05-28 15:30:04 +0200

    fixed typo at line 33 with addeventlistener

--- week-08/day-2/candyShop/game.js
+++ week-08/day-2/candyShop/game.js
@@ -1,6 +1,9 @@
 'use strict';
 
+let i = 0;
+
 let creatCandie = document.querySelector('.create-candies');
+
 creatCandie.addEventListener('click', () => {
     let candies = parseInt(document.querySelector('.candies').innerHTML);
     candies++
@@ -15,37 +18,22 @@ buyLollipooop.addEventListener('click', () => {
         document.querySelector('.candies').innerHTML = candies;
         document.querySelector('.lollypops').innerHTML += '🍭';
         i = Math.floor(document.querySelector('.lollypops').innerHTML.length / 20);
-        document.querySelector('.speed').innerHTML = i;
+        document.querySelector('.speed').innerHTML = i
     }
 });
 
-let i = Math.floor(document.querySelector('.lollypops').innerHTML.length / 20);
-document.querySelector('.speed').innerHTML = i;
 setInterval(() => {
     let candies = parseInt(document.querySelector('.candies').innerHTML);
     candies += i
     document.querySelector('.candies').innerHTML = candies;
 }, 1000)
 
+// If you press the "make candy rain" button, the candy generation should speed up 10x
 
-
-// // If you press the "make candy rain" button, the candy generation should speed up 10x
-// let disable = ()=>{
-// buyLollipooop.disabled=true;
-// };
-
-// document.querySelector('.candy-machine'), addEventListener('click', () => {
-//     i = i * 10
-//     let candies = parseInt(document.querySelector('.candies').innerHTML);
-//     candies += i
-//     document.querySelector('.candies').innerHTML = candies;
-//     document.querySelector('.speed').innerHTML = i
-//     buyLollipooop.disable()
-//     setTimeout(() => {
-//         i = Math.floor(document.querySelector('.lollypops').innerHTML.length / 20);
-//     }, 2000)
-//     setTimeout(()=>{
-//         document.querySelector('.speed').innerHTML = i
-//         buyLollipooop.disabled=false;
-//     }, 2001);
-// });
+document.querySelector('.candy-machine').addEventListener('click', () => {
+    let j = i * 10
+    document.querySelector('.speed').innerHTML = j
+    setTimeout(()=>{
+    document.querySelector('.speed').innerHTML = i
+    },3000)
+});
