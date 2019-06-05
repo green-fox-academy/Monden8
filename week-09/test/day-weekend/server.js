e192582296a264a484b692f88e39310b2d3945da
Soma <soma.feigl@gmail.com>
2019-06-05 11:31:12 +0200

    added app.post

--- week-09/test/day-weekend/server.js
+++ week-09/test/day-weekend/server.js
@@ -7,7 +7,7 @@ const eskweel = require('mysql');
 const path = require('path');
 require('dotenv').config();
 
-app.use('/public',exp.static('public'));
+app.use('/public', exp.static('public'));
 app.use(exp.json());
 
 const conn = eskweel.createConnection({
@@ -23,27 +23,60 @@ conn.connect(err => {
     }
     console.log('connection to DB is OK ✨');
 });
-
+//---------------------------------------------------------------
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/public/index.html')
-  });
-
-app.post('/api/links',(req,res)=>{
-    console.log(req.body);
+    res.sendFile(__dirname + '/public/index.html')
 });
+//---------------------------------------------------------------
+app.post('/api/links', (req, res) => {
+    const { url, alias } = req.body;
+    conn.query(
+        'SELECT * FROM URL_Aliasing Where alias = ?',
+        [alias || ''], (err, rows) => {
+            if (err) {
+                console.error(err);
+            } else if (rows.length > 0) {
+                // alias is already taken WRITE IT OUT Your alias is already in use! //red
+                res.status(405).send();
+            } else {
+                // create one
+                let secretCode = generateSecretCode();
+                conn.query(
+                    'INSERT INTO URL_Aliasing (url, alias, secret_code) VALUES (?, ?, ?)',
+                    [url, alias, secretCode],
+                    (err, rows) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            // read back and WRITE OUT "Your URL is aliased to {alias} and your secret code is {secretCode}.""
+                            conn.query(
+                                'SELECT * FROM URL_Aliasing WHERE alias = ?',
+                                [alias || ''],
+                                (err, rows) => {
+                                    if (err) {
+                                        console.error(err);
+                                    } else {
+                                        res.status(200).send(rows[0]);
+                                    }
+                                },
+                            );
+                        }
+                    },
+                );
+            }
+        },
+    );
+});
+//-----------------------------------------------
+function generateSecretCode() {
+    return `${getARandomNumber(0, 9)}${getARandomNumber(0, 9)}${getARandomNumber(0, 9)}${getARandomNumber(0, 9)}`;
+}
+
+function getARandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT} 🔥`);
-  });
+});
