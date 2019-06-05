aad8ffa076768797d609ad98568761cd9dee0977
Soma <soma.feigl@gmail.com>
2019-06-05 14:01:01 +0200

    added http checker and empty input checker

--- week-09/test/day-weekend/public/front.js
+++ week-09/test/day-weekend/public/front.js
@@ -11,19 +11,25 @@ function getFormData() {
 
 document.querySelector('button').addEventListener('click', (event) => {
   event.preventDefault();
-  XML.open('POST', '/api/links', true);
-  XML.setRequestHeader('Content-Type', 'application/json');
-  XML.onload = (data) => {
-    let feedback = document.querySelector('p');
-    if (data.srcElement.status == 405) {
-      feedback.style.color = 'red';
-      feedback.innerHTML = data.srcElement.response;
-    } else {
-      document.querySelectorAll('input')[0].value = '';
-      document.querySelectorAll('input')[1].value = '';
-      feedback.style.color = 'black';
-      feedback.innerHTML = data.srcElement.response;
-    }
-  };
-  XML.send(JSON.stringify(getFormData()));
+  if (document.querySelectorAll('input')[0].value == '' || document.querySelectorAll('input')[1].value == '') {
+    window.alert('Please fill out both fields!');
+  } else if (document.querySelectorAll('input')[0].value.match(/http/) !== null) {
+    XML.open('POST', '/api/links', true);
+    XML.setRequestHeader('Content-Type', 'application/json');
+    XML.onload = (data) => {
+      let feedback = document.querySelector('p');
+      if (data.srcElement.status == 405) {
+        feedback.style.color = 'red';
+        feedback.innerHTML = data.srcElement.response;
+      } else {
+        document.querySelectorAll('input')[0].value = '';
+        document.querySelectorAll('input')[1].value = '';
+        feedback.style.color = 'black';
+        feedback.innerHTML = data.srcElement.response;
+      }
+    };
+    XML.send(JSON.stringify(getFormData()));
+  } else {
+    window.alert('Please type in a valid url!');
+  }
 });
